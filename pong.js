066b4f108274bfe254db41feed0584da76051434
FredericMentel <frederic.mentel@gmail.com>
2019-05-01 09:50:04 +0200

    add a ball

---
+++ pong.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+
+// add black background
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+//add a ball
+context.fillStyle = '#fff';
+context.fillRect(0, 0, 10, 10);
+

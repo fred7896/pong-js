d544b01dc7a3922ac7b03dbf458efbbcf4223b69
FredericMentel <frederic.mentel@gmail.com>
2019-05-01 12:12:25 +0200

    build players

--- pong.js
+++ pong.js
@@ -31,6 +31,16 @@ class Ball extends Rect {
   }
 }
 
+class Player extends Rect 
+{
+    constructor()
+    {
+        super(20,100);
+        this.score = 0;
+    }
+
+}
+
 class Pong {
   constructor(canvas) {
     this._canvas = canvas;
@@ -43,36 +53,43 @@ class Pong {
     this.ball.vel.x = 100;
     this.ball.vel.y = 100;
 
+    this.players = [new Player, new Player];
+
+    this.players[0].pos.x = 40;
+    this.players[1].pos.x = this._canvas.width - 40;
+
+    this.players.forEach(player => {
+        player.pos.y = this._canvas.height / 2;
+    });
 
     let lastTime;
 
     const callback = millis => {
-    if (lastTime) {
+      if (lastTime) {
         this.update((millis - lastTime) / 1000);
-    }
-    lastTime = millis;
-    requestAnimationFrame(callback);
+      }
+      lastTime = millis;
+      requestAnimationFrame(callback);
     };
 
     callback();
   }
-  draw()
-  {
+  draw() {
     // add black background
     this._context.fillStyle = "#000";
     this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
 
     this.drawRect(this.ball);
+    // add Players
+    this.players.forEach(player => this.drawRect(player));
   }
-  drawRect(rect)
-  {
+  drawRect(rect) {
     //add a ball
     this._context.fillStyle = "#fff";
-    this._context.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y);
+    this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
   }
   // dt = delta time
-  update(dt) 
-  {
+  update(dt) {
     this.ball.pos.x += this.ball.vel.x * dt;
     this.ball.pos.y += this.ball.vel.y * dt;
 
@@ -84,13 +101,9 @@ class Pong {
     }
 
     this.draw();
-
-  };
+  }
 }
 
 const canvas = document.getElementById("pong");
 
 const pong = new Pong(canvas);
-
-
-

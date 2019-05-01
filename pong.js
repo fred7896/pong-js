b52ec6af3f0cadd601ea2c5b9197121f02c3ca09
FredericMentel <frederic.mentel@gmail.com>
2019-05-01 12:46:47 +0200

    create reset method and initialize counter score

--- pong.js
+++ pong.js
@@ -47,11 +47,7 @@ class Pong {
     this._context = canvas.getContext("2d");
 
     this.ball = new Ball();
-    this.ball.pos.x = 100;
-    this.ball.pos.y = 50;
 
-    this.ball.vel.x = 100;
-    this.ball.vel.y = 100;
 
     this.players = [new Player, new Player];
 
@@ -73,6 +69,14 @@ class Pong {
     };
 
     callback();
+
+    this.reset();
+  }
+  collide(player, ball) 
+  {
+      if(player.left < ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top) {
+          ball.vel.x = -ball.vel.x;
+      }
   }
   draw() {
     // add black background
@@ -88,19 +92,30 @@ class Pong {
     this._context.fillStyle = "#fff";
     this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
   }
+  reset()
+  {
+    this.ball.pos.x = this._canvas.width / 2;
+    this.ball.pos.y = this._canvas.height / 2;
+    this.ball.vel.x = 0;
+    this.ball.vel.y = 0;
+  }
   // dt = delta time
   update(dt) {
     this.ball.pos.x += this.ball.vel.x * dt;
     this.ball.pos.y += this.ball.vel.y * dt;
 
     if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-      this.ball.vel.x = -this.ball.vel.x;
+      const playerId = this.ball.vel.x < 0 | 0;
+      this.players[playerId].score++;
+      this.reset();
     }
     if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
       this.ball.vel.y = -this.ball.vel.y;
     }
 
     this.players[1].pos.y = this.ball.pos.y;
+
+    this.players.forEach(player => this.collide(player, this.ball));
 
     this.draw();
   }
